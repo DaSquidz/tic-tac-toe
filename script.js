5a11370cbe672d62a738c2ae59a6c8cd0a1e5b76
Riccardo Geri <geririccardo34@gmail.com>
2021-03-20 21:14:38 +0100

    Add gameBoard module

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//module pattern:
+// const gameBoard = (() => {
+    
+// })();
+
+
+const gameBoard = (() => {
+    const marksArray = ["","","","","","","","",""];
+
+    const addMark = (position, mark) => {
+        marksArray[index] = mark;
+    };
+    const resetGrid = () => {
+        for(let i = 0; i < marksArray.length; i++) {
+            marksArray[i] = "";
+        }
+    }
+
+})();
+
+const UIChanger = (() => {
+    const gridSquares = document.querySelectorAll(".game-grid-square");
+
+})();
